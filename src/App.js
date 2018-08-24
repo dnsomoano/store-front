6db4d8724a564ca71337d86bc2cf9e8f26091b89
Daniel N Somoano <dnsomoano@gmail.com>
2018-08-24 19:05:43 -0400

    Worked on side bar, must add content

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          Welcome to my store!
+          <ul>
+            <li>
+              <select>
+                <option value="accessories">Accessories</option>
+                <option value="cases">Cases</option>
+                <option value="cpu">CPUs</option>
+                <option value="gpu">Graphic Cards</option>
+                <option value="fans">Fans/Water cooling</option>
+                <option value="hard-drives">Hard Drives</option>
+                <option value="motherboard">Motherboards</option>
+                <option value="power-supply">Power Supplies</option>
+                <option value="ram">Ram</option>
+              </select>
+            </li>
+          </ul>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <section className="body">
+          <section className="main-container">
+            <section className="featured-products">featured products</section>
+            <section className="nav-bar">
+            <ul>
+              <li><a className="nav-button" value="accessories">Accessories</a></li>
+              <li><a className="nav-button" value="cases">Cases</a></li>
+              <li><a className="nav-button" value="cpu">CPUs</a></li>
+              <li><a className="nav-button" value="gpu">Graphic Cards</a></li>
+              <li><a className="nav-button" value="fans">Fans/Water cooling</a></li>
+              <li><a className="nav-button" value="hard-drives">Hard Drives</a></li>
+              <li><a className="nav-button" value="motherboard">Motherboards</a></li>
+              <li><a className="nav-button" value="power-supply">Power Supplies</a></li>
+              <li><a className="nav-button" value="ram">Ram</a></li>
+            </ul>
+            </section>
+          </section>
+        </section>
       </div>
     );
   }
